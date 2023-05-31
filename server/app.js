e3f21e1a3bf96ddfc0cc9449330b8ed298805f7f
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-05-30 23:26:48 -0400

    intentando hacer deploy

--- server/app.js
+++ server/app.js
@@ -30,13 +30,13 @@ app.listen(3000, console.log('SERVIDOR ENCENDIDO'));
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://stirring-praline-60de60.netlify.app'
+  origin: 'https://main--gorgeous-belekoy-7f32eb.netlify.app/'
 }));
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://stirring-praline-60de60.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://main--gorgeous-belekoy-7f32eb.netlify.app/');
   // Pass to next layer of middleware
   next();
 });
