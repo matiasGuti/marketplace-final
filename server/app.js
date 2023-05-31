a4c3f0a42d6981697be823457b6ded9caf872363
matiasGutierrez <m.ignacio.guti@gmail.com>
2023-05-30 23:25:25 -0400

    new build

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ const app = express();
 app.listen(3000, console.log('SERVIDOR ENCENDIDO'));
 
 app.use(express.json());
+
 app.use(cors({
   origin: 'https://stirring-praline-60de60.netlify.app'
 }));
@@ -36,17 +37,6 @@ app.use(cors({
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'https://stirring-praline-60de60.netlify.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
   // Pass to next layer of middleware
   next();
 });
